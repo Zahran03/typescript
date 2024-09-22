303563761751ca983803199474b8c52c357a905e
Zahran03 <zalfarizi502@gmail.com>
2024-09-22 21:21:58 +0700

    lesson 1

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const nama = "Zahran";
+let age = 11;
+console.log(nama);
+console.log(age);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const nama: string = "Zahran";
+let age: number = 11;
+console.log(nama);
+console.log(age);
