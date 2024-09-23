8c67d0c19893e546327aa7a2cdb406c98f0dfe40
Zahran03 <zalfarizi502@gmail.com>
2024-09-23 13:40:49 +0700

    lesson3

---
+++ dist/Array_Object.js
@@ -0,0 +1,20 @@
+"use strict";
+// Array
+let names = ["Zahran", "Komputer", "Mouse"];
+let ages = [21, 22, 23];
+names.push("Layla");
+ages.push(25);
+// Type inference with Arrays
+let fruits = ["Banana", "Apple", "Cherry", "Mango"];
+fruits.push("Avocado");
+const f = fruits[1];
+let things = [1, "fruits", true];
+const t = things[0];
+// Object Literals
+let user = {
+    firstName: "zahran",
+    age: 21,
+    id: 2,
+};
+user.firstName = "rizky";
+user.id = 2;
