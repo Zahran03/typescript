84151a3d102b4501d45fefe7a1ffb2a61ba8d23d
Zahran03 <zalfarizi502@gmail.com>
2024-09-23 13:14:13 +0700

    Lesson 3

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,20 @@
 "use strict";
-const nama = "Zahran";
-let age = 11;
-console.log(nama);
-console.log(age);
+// Array
+let names = ["Zahran", "Komputer", "Mouse"];
+let ages = [21, 22, 23];
+names.push("Layla");
+ages.push(25);
+// Type inference with Arrays
+let fruits = ["Banana", "Apple", "Cherry", "Mango"];
+fruits.push("Avocado");
+const f = fruits[1];
+let things = [1, "fruits", true];
+const t = things[0];
+// Object Literals
+let user = {
+    firstName: "zahran",
+    age: 21,
+    id: 2,
+};
+user.firstName = "rizky";
+user.id = 2;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-const nama: string = "Zahran";
-let age: number = 11;
-console.log(nama);
-console.log(age);
-console.log("hello world");
+// Array
+let names: string[] = ["Zahran", "Komputer", "Mouse"];
+let ages: number[] = [21, 22, 23];
+
+names.push("Layla");
+ages.push(25);
+
+// Type inference with Arrays
+let fruits = ["Banana", "Apple", "Cherry", "Mango"];
+fruits.push("Avocado");
+const f = fruits[1];
+
+let things = [1, "fruits", true];
+const t = things[0];
+
+// Object Literals
+
+let user: { firstName: string; age: number; id: number } = {
+  firstName: "zahran",
+  age: 21,
+  id: 2,
+};
+
+user.firstName = "rizky";
+user.id = 2;
