10cd9510d6bd9e39fbdce7bee18b55b3e7faecf8
Zahran03 <zalfarizi502@gmail.com>
2024-09-24 20:15:31 +0700

    lesson5

--- src/Any.ts
+++ src/Any.ts
@@ -1,16 +1,23 @@
-"use strict";
 // Any in variabel
-let sesuatu = "Makan";
+
+let sesuatu: any = "Makan";
+
 sesuatu = 12;
 sesuatu = "berenang";
+
 // Any in Array
-let anything = ["buah", 12, true];
+
+let anything: any[] = ["buah", 12, true];
+
 anything.push("Anda");
 anything.push(12);
+
 // function & any
-function addTogether(value) {
-    return value + value;
+
+function addTogether(value: any): any {
+  return value + value;
 }
+
 const resultOne = addTogether("hello");
 console.log(resultOne);
 const resultTwo = addTogether(2);
